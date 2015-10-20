7c2caff590054a798a136ff978b68ff20216d52a
shurup74 <shurup74@mail.ru>
2015-10-20 14:48:21 -0400

    first upload

---
+++ App_Code/AudioLink.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for AudioLink
+/// </summary>
+public class AudioLink
+{
+    public AudioLink(){}
+    public string LinkSource { get; set; }
+    public string LinkName { get; set; }
+}
